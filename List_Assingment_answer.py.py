cb406222a422195506a2de70256b59af9d9ebb0b
ankit kumar <ankitsharma2595@gmail.com>
2018-10-11 16:26:25 +0530

    version 1

---
+++ List_Assingment_answer.py.py
@@ -0,0 +1,47 @@
+#1. Write a Python program to print the numbers of a specified list after removing even numbers from it.
+
+x1= [10,20,30,1,3,5,6]
+x2= x1.remove(i%2 ==0)
+print x2
+
+
+
+
+#2. Write a Python program to convert a list of characters into a string.
+
+lis1 = ['1','2','3','4']
+
+''.join(lis1)
+
+
+#3. Write a Python program to append a list to the second list.
+
+l1= [1,2,3,4]
+l2 = [5,6,7]
+
+l1.append(l2)
+
+print l1
+
+
+
+#4. Write a Python program to sum all the items in a list.
+
+l1 = sum([1,2,3,4,5])
+
+
+
+#5. Write a Python program to multiplies all the items in a list.
+
+
+
+#6. Write a Python program to get the largest number from a list.
+listt1= [10,20,30,40,50]
+print max(listt1) 
+
+
+
+# 7. Write a Python program to get the smallest number from a list.
+listtt1= [10,20,30,40,50,60,70,5,6,7,1]
+print min(listtt1)
+  
